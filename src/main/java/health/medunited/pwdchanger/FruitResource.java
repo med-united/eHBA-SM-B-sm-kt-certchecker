69f911af74f203ebbf8c34398139fe466ef22a4b
Pablo Escaida <1pablo.escaida1@gmail.com>
2023-02-23 20:57:47 +0100

    minimized the function and made a returnable string

--- src/main/java/health/medunited/pwdchanger/FruitResource.java
+++ src/main/java/health/medunited/pwdchanger/FruitResource.java
@@ -4,35 +4,28 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Set;
 
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
 
 @Path("/fruits")
 public class FruitResource {
 
-    private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
-
-    public FruitResource() {
-        fruits.add(new Fruit("Apple", "Winter fruit"));
-        fruits.add(new Fruit("Pineapple", "Tropical fruit"));
-    }
-
     @GET
-    public Set<Fruit> list() {
-        return fruits;
+    @Path("/status")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getPinInfo(String medkationsPlan) {
+
+        return "Hola!!!";
     }
 
     @POST
     public Set<Fruit> add(Fruit fruit) {
-        fruits.add(fruit);
-        return fruits;
+        return null;
     }
 
     @DELETE
     public Set<Fruit> delete(Fruit fruit) {
-        fruits.removeIf(existingFruit -> existingFruit.name.contentEquals(fruit.name));
-        return fruits;
+        return null;
     }
 }
