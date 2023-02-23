fd9a8ebc178072e7bc50ea5d4b2b869070c579c2
Pablo Escaida <1pablo.escaida1@gmail.com>
2023-02-23 21:01:26 +0100

    removed old tests

--- src/test/java/health/medunited/pwdchanger/FruitResourceIT.java
+++
@@ -1,8 +0,0 @@
-package health.medunited.pwdchanger;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class FruitResourceIT extends FruitResourceTest {
-
-}
