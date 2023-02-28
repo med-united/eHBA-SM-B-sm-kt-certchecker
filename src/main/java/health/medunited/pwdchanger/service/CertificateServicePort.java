bc6267a4b609cbe9d86ffa7ef196527543eac356
Pablo Escaida <1pablo.escaida1@gmail.com>
2023-02-28 17:35:43 +0100

    readCertificate part1 finished

--- src/main/java/health/medunited/pwdchanger/service/CertificateServicePort.java
+++ src/main/java/health/medunited/pwdchanger/service/CertificateServicePort.java
@@ -1,16 +1,19 @@
 package health.medunited.pwdchanger.service;
 
 import de.gematik.ws.conn.cardservice.v8.PinStatusEnum;
-import de.gematik.ws.conn.cardservice.wsdl.v8.CardService;
+import de.gematik.ws.conn.certificateservice.v6.ReadCardCertificate;
 import de.gematik.ws.conn.certificateservice.v6.ReadCardCertificateResponse;
 import de.gematik.ws.conn.certificateservice.wsdl.v6.CertificateService;
 import de.gematik.ws.conn.certificateservice.wsdl.v6.CertificateServicePortType;
+import de.gematik.ws.conn.certificateservicecommon.v2.X509DataInfoListType;
 import de.gematik.ws.conn.connectorcommon.v5.Status;
 import de.gematik.ws.conn.connectorcontext.v2.ContextType;
 import health.medunited.pwdchanger.model.gematik.GetPinStatusResponse;
+import health.medunited.pwdchanger.security.BindingProviderConfigurer;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.TrustManager;
+import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Holder;
 import java.math.BigInteger;
 
@@ -21,32 +24,44 @@ public class CertificateServicePort {
 
     private final ContextType context;
 
+    private final String cardHandle = "abc1";
+
+    private final ReadCardCertificate.CertRefList certRefList;
+
     public CertificateServicePort(String endpoint, ContextType context, TrustManager trustManager, HostnameVerifier hostnameVerifier) {
         this.context = context;
         this.certificateServicePortType = new CertificateService(getClass()
                 .getResource("/CertificateService.wsdl"))
                 .getCertificateServicePort();
+        BindingProvider bp = (BindingProvider) certificateServicePortType;
+        bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endpoint);
+        BindingProviderConfigurer.configure(bp, trustManager, hostnameVerifier);
+        certRefList = null;
     }
 
+    public ReadCardCertificateResponse readCardCertificate(ContextType context, String cardHandle,  ReadCardCertificate.CertRefList certRefList) {
 
-
-    public ReadCardCertificateResponse readCardCertificate() {
-        /* Parameters
-        CardHandle
-        Context
-        CertRefList
+        /*
+        Return Parameters:
+        Status
+        CertRef
+        X509Data
+        X509IssuerName
+        509SerialNumber
+        X509SubjectName
+        X509BaseCertificateId
+        X509Certificate
          */
 
         Holder<Status> status = new Holder<>();
-        Holder<PinStatusEnum> pinResultEnum = new Holder<>();
-        Holder<BigInteger> leftTries = new Holder<>();
-        /*
+        Holder<X509DataInfoListType> certList = new Holder<>();
+
         try {
-            this.cardServicePortType.getPinStatus(context, cardHandle, "PIN.QES", status, pinResultEnum, leftTries);
-            return new GetPinStatusResponse(status.value, pinResultEnum.value, leftTries.value);
-        } catch (de.gematik.ws.conn.cardservice.wsdl.v8.FaultMessage e) {
+            this.certificateServicePortType.readCardCertificate(cardHandle, context, certRefList, status, certList);
+            return new ReadCardCertificateResponse();
+        } catch (de.gematik.ws.conn.certificateservice.wsdl.v6.FaultMessage e) {
             e.printStackTrace();
-        }*/
+        }
         return null;
 
     }
@@ -57,5 +72,9 @@ public class CertificateServicePort {
 
     public CertificateServicePortType getCertificateServicePortType() {
         return certificateServicePortType;
+    }
+
+    public ContextType getContext() {
+        return context;
     }
 }
