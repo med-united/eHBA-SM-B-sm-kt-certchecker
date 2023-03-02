f025be97511926dd2693db6bdd40ea7f0ab0a6ed
Pablo Escaida <1pablo.escaida1@gmail.com>
2023-03-02 18:50:27 +0100

    major files compiling SincleConnector DefaultConnector MultiConnector

--- src/main/java/health/medunited/pwdchanger/service/DefaultConnectorServicesProvider.java
+++ src/main/java/health/medunited/pwdchanger/service/DefaultConnectorServicesProvider.java
@@ -1,26 +1,26 @@
-//package health.medunited.pwdchanger.service;
-//
-//import javax.annotation.PostConstruct;
-//import javax.enterprise.context.ApplicationScoped;
-//import javax.inject.Inject;
-//
-//import health.medunited.pwdchanger.config.UserConfig;
-//import health.medunited.pwdchanger.provider.AbstractConnectorServicesProvider;
-//
-//@ApplicationScoped
-//public class DefaultConnectorServicesProvider extends AbstractConnectorServicesProvider {
-//
-//    @Inject
-//    UserConfig userConfig;
-//
-//    @PostConstruct
-//    void init() {
-//        initializeServices();
-//    }
-//
-//    @Override
-//    public UserConfig getUserConfig() {
-//        return userConfig;
-//    }
-//
-//}
+package health.medunited.pwdchanger.service;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import health.medunited.pwdchanger.config.UserConfig;
+import health.medunited.pwdchanger.provider.AbstractConnectorServicesProvider;
+
+@ApplicationScoped
+public class DefaultConnectorServicesProvider extends AbstractConnectorServicesProvider {
+
+    @Inject
+    UserConfig userConfig;
+
+    @PostConstruct
+    void init() {
+        initializeServices();
+    }
+
+    @Override
+    public UserConfig getUserConfig() {
+        return userConfig;
+    }
+
+}
